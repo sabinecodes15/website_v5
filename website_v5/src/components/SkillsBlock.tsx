4af311a5af8273e140671ce27b7a2d69756ce8aa
Sabine Salnave <sdsalnave@iCloud.com>
2024-08-08 17:13:56 -0400

    added border to skills

--- website_v5/src/components/SkillsBlock.tsx
+++ website_v5/src/components/SkillsBlock.tsx
@@ -6,43 +6,51 @@ const SkillsBlock = () => {
       <div className="flex flex-row">
         <div className="m-2 basis-1/3">
           <Card size="span">
-            <div className="flex flex-col text-light-blue-100 font-mont items-center">
-              <h3 className="text-white font-bold tracking-wide">Frontend</h3>
-              <div className="h-2"></div>
-              <p>React</p>
-              <p>HTML</p>
-              <p>CSS</p>
-              <p>Javascript</p>
-              <p>Figma</p>
-              <p>JQuery</p>
+            <div className="m-4 border border-light-blue-100 rounded">
+              <div className="flex flex-col text-light-blue-100 font-mont items-center m-4">
+                <h3 className="text-white font-bold tracking-wide">Frontend</h3>
+                <div className="h-2"></div>
+                <p>React</p>
+                <p>HTML</p>
+                <p>CSS</p>
+                <p>Javascript</p>
+                <p>Figma</p>
+                <p>JQuery</p>
+              </div>
             </div>
           </Card>
         </div>
         <div className="m-2 basis-1/3">
           <Card size="span">
-            <div className="flex flex-col text-light-blue-100 font-mont items-center">
-              <h3 className="text-white font-bold tracking-wide">Backend</h3>
-              <div className="h-2"></div>
-              <p>Python</p>
-              <p>Java</p>
-              <p>Ruby</p>
-              <p>Ruby on Rails</p>
-              <p>C</p>
-              <p>MySQL</p>
+            <div className="m-4 border border-light-blue-100 rounded">
+              <div className="flex flex-col text-light-blue-100 font-mont items-center m-4">
+                <h3 className="text-white font-bold tracking-wide">Backend</h3>
+                <div className="h-2"></div>
+                <p>Python</p>
+                <p>Java</p>
+                <p>Ruby</p>
+                <p>Ruby on Rails</p>
+                <p>C</p>
+                <p>MySQL</p>
+              </div>
             </div>
           </Card>
         </div>
         <div className="m-2 basis-1/3">
           <Card size="span">
-            <div className="flex flex-col text-light-blue-100 font-mont items-center">
-              <h3 className="text-white font-bold tracking-wide">
-                Infrastructure
-              </h3>
-              <div className="h-2"></div>
-              <p>AWS</p>
-              <p>Docker</p>
-              <p>Kubernetes</p>
-              <p>Terraform</p>
+            <div className="m-4 border border-light-blue-100 rounded">
+              <div className="flex flex-col text-light-blue-100 font-mont items-center m-4">
+                <h3 className="text-white font-bold tracking-wide">
+                  Infrastructure
+                </h3>
+                <div className="h-2"></div>
+                <p>AWS</p>
+                <p>Docker</p>
+                <p>Kubernetes</p>
+                <p>Terraform</p>
+                <p className="text-teal-300">_____</p>
+                <p className="text-teal-300">_____</p>
+              </div>
             </div>
           </Card>
         </div>
