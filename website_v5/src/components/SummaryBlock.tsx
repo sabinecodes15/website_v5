e55d0240441628d83770066bbd1502d48f3ab6e0
Sabine Salnave <sdsalnave@iCloud.com>
2024-08-17 14:05:57 -0400

    changing summary block size

--- website_v5/src/components/SummaryBlock.tsx
+++ website_v5/src/components/SummaryBlock.tsx
@@ -1,21 +1,22 @@
 const SummaryBlock = () => {
   return (
-    <div className="w-full min-h-screen bg-teal-300 flex flex-col sm:gap-12">
-      <h1 className="text-white-100 font-mono font-bold text-5xl tracking-wide mt-12 mx-6 sm:mx-12">
-        Who I am.
-      </h1>
-      <p className="mx-6 sm:mx-12 my-12 text-light-blue-100 font-mont text-lg sm:text-2xl lg:text-3xl tracking-wide leading-loose">
-        I'm a recent CS grad from the University of Chicago, and I'm passionate
-        about making people's lives easier. I love leveraging my technical
-        abilities to build clean, well-thought-out solutions for challenging,
-        irritating, and inconvenient problems. Having a multitude of
-        technological interests, I tend to gravitate toward roles related to
-        full stack and cloud infrastructure development. I'm not afraid to wear
-        multiple hats or learn new skills— I'm eager to pick up whatever is
-        necessary to get the job done.
-      </p>
+    <div className="w-full min-h-fit bg-teal-300">
+      <div className="mx-6 sm:mx-12 py-12 flex flex-col sm:gap-12 gap-6">
+        <h1 className="text-white-100 font-mono font-bold text-5xl tracking-wide">
+          Who I am.
+        </h1>
+        <p className="text-light-blue-100 font-mont text-lg sm:text-2xl lg:text-3xl tracking-wide leading-loose">
+          I'm a recent CS grad from the University of Chicago, and I'm
+          passionate about making people's lives easier. I love leveraging my
+          technical abilities to build clean, well-thought-out solutions for
+          challenging, irritating, and inconvenient problems. Having a multitude
+          of technological interests, I tend to gravitate toward roles related
+          to full stack and cloud infrastructure development. I'm not afraid to
+          wear multiple hats or learn new skills— I'm eager to pick up whatever
+          is necessary to get the job done.
+        </p>
+      </div>
     </div>
   );
 };
-
 export default SummaryBlock;
